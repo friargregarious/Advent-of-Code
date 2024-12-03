7a2005e07e28ab0b29e48fb269213f4366504f87
Gregory Denyes <greg.denyes@gmail.com>
2024-12-03 18:19:04 -0500

    archived day 09 of 2023

--- src/AOC 2023/Day 09/Solve_2023_09 OLD.py
+++ src/AOC 2023/Day 09/Solve_2023_09 OLD.py
@@ -20,7 +20,6 @@
 
 import os
 from configparser import ConfigParser
-from pathlib import Path
 
 # import math
 # from datetime import datetime
@@ -38,27 +37,20 @@ from tools import Prediction, line_results
 ###############################################################################
 # PART A ######################################################################
 ###############################################################################
+class my_runner:
+    total = 0
 
 def solve_a(source):
     """Oasis And Sand Instability Sensor"""
-    prediction_sum = 0
-    for sequence in source:
+    # first we extrapolate till zeros
+    running_total = my_runner()
+    answers = [Prediction(page, running_total).answer for page in source]
+    print("# of predictions:", len(answers))
+    open("lineresults.txt", "w").write("\n".join(line_results))
+    solution = sum(answers)
 
-        while True:
-            # Calculate the differences between consecutive numbers in the sequence
-            differences = [sequence[i] - sequence[i-1] for i in range(1, len(sequence))]
+    return solution
 
-            # If the differences are all zeroes, we can predict the next value
-            if all(d == 0 for d in differences):
-
-                # The next value is the last value in the sequence plus the last difference
-                next_value = sequence[-1] + differences[-1]
-                prediction_sum += next_value
-
-            # Otherwise, repeat the process with the differences as the new sequence
-            sequence = differences
-
-    return prediction_sum
 
 ###############################################################################
 # PART B ######################################################################
@@ -76,11 +68,8 @@ def parse(source):
     """parses the list of strings into integers"""
     sensor_history = []
     for reading in source:
-        # print(reading)
-        # if len(reading) > 1:
-        converted = list(map(int, reading.split(" ")))
-        # print(converted)        
-        sensor_history.append( converted )
+        if len(reading) > 1:
+            sensor_history.append([int(x) for x in reading.split(" ")])
     return sensor_history
 
 
@@ -91,6 +80,60 @@ def main(raw):
     return (solve_a(source=processed), solve_b(source=processed))
 
 
+###############################################################################
+# RUNNING FROM HOME ###########################################################
+###############################################################################
 if __name__ == "__main__":
-    source = Path("input.txt").read_text().split("\n")
-    print(solve_a(parse(source)))
+    THISYEAR, THISDAY = 2023, 9
+    # set the example flag here
+    EXAMPLE = False
+    # EXAMPLE = True
+    C_ANS_A = 114
+    C_ANS_B = 6
+
+    os.system("cls")
+    # root = os.getcwd().replace("\\", "/")
+    # exe_msg = f"{root}/my_utilities/reporting.py -y {THISYEAR} -d {THISDAY}"
+
+    # if EXAMPLE:
+    #     exe_msg += f" -e '{C_ANS_A},{C_ANS_B}'"
+
+    # os.system(exe_msg)
+
+    if EXAMPLE:
+        data = open("example.txt", encoding="utf-8").read()
+    else:
+        data = open("input.txt", encoding="utf-8").read()
+
+    final_answer_a, final_answer_b = main(data)
+
+    if EXAMPLE:
+        for row in [("A", C_ANS_A, final_answer_a), ("B", C_ANS_B, final_answer_b)]:
+            part, c_answer, my_answer = row
+            is_good = my_answer == c_answer
+
+            if is_good:
+                msg = f"Your answer for {part}: {my_answer} is CORRECT!!"
+                CLR = "green"
+
+            if not is_good:
+                msg = f"Your answer for {part}: {my_answer} is NOT CORRECT!!\n"
+                msg += f"The correct answer is {c_answer}"
+                CLR = "red"
+
+            print(colored(msg, color=CLR))
+
+    else:
+        cfg = ConfigParser()
+        cfg.read("C:/Advent of Code/.unused/.env")
+        token = cfg.get(section="friargregarious", option="token")
+        me = aocd.models.User(token=token)
+        this_puzzle = aocd.models.Puzzle(THISYEAR, THISDAY, user=me)
+
+        if not this_puzzle.answered_a:
+            this_puzzle.answer_a = final_answer_a
+
+        elif this_puzzle.answered_a and not this_puzzle.answered_b:
+            this_puzzle.answer_b = final_answer_b
+        else:
+            print("You've answered these puzzles already!")
